b8cc0a0bc57f4df360de00b337a751239d79f350
yanlaijun <yanlaijun@xiaomi.com>
2015-07-25 16:43:00 +0800

    local branch(delete)

--- hello.c
+++ hello.c
@@ -6,4 +6,5 @@ int main(int argc, char *argv[][])
     printf("Hello world, Git Again,Again, Again!-dev branch.---in master\n");
     printf("heloo world, 2015.7.25...\n");
     printf("hello world, my branch");
+    printf("hello world, local branch(delete)");
 }
