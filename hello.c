f4ed0fe00e44754a09e95d5eee0115f4c5b5902b
yanlaijun <yanlaijun@xiaomi.com>
2015-07-25 16:20:37 +0800

    modified in 2015.07.25-ylj

--- hello.c
+++ hello.c
@@ -4,5 +4,5 @@ int main(int argc, char *argv[][])
     printf("Hello world, Git Again,Again, Again!\n");
     printf("Hello world, Git Again,Again, Again!----master branch\n");
     printf("Hello world, Git Again,Again, Again!-dev branch.---in master\n");
-
+    printf("heloo world, 2015.7.25...\n");
 }
