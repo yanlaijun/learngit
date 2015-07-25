7af4b3fe0e6e74554a233b21c69ffc30be5979e3
yanlaijun <yanlaijun@xiaomi.com>
2015-07-25 16:46:44 +0800

    mybranch my delete

--- hello.c
+++ hello.c
@@ -5,5 +5,5 @@ int main(int argc, char *argv[][])
     printf("Hello world, Git Again,Again, Again!----master branch\n");
     printf("Hello world, Git Again,Again, Again!-dev branch.---in master\n");
     printf("heloo world, 2015.7.25...\n");
-    printf("hello world, my branch");
+    printf("hello world, my branch(my branche delete)");
 }
